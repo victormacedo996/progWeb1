dad3b8956d3497f1c32251ab7abac1219e54055b
ADS 2021.1N <36124872021.1N@senacrjedu.local>
2022-08-11 21:31:21 -0300

    primeiro commit

---
+++ src/main/java/br/com/senac/ProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringApplication.class, args);
+	}
+
+}
