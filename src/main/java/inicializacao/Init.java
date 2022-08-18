db703c272b46bdc0b1bcc81722f57f5887b94fed
JOHNNY EDWARD VILLAVICENCIO TAFUR <johnny.tafur@educacao.senac.rio>
2022-08-18 20:43:57 -0300

    feat: added init class

---
+++ src/main/java/inicializacao/Init.java
@@ -0,0 +1,55 @@
+package inicializacao;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import br.com.senac.entity.Aluno;
+
+import br.com.senac.service.AlunoService;
+
+@Component
+public class Init implements ApplicationListener<ContextRefreshedEvent> {
+    
+    @Autowired
+    AlunoService alunoService;
+
+    //@Autowired
+    //AlunoRepository repo;
+
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event){
+
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("Lucas");
+
+        Aluno aluno2 = new Aluno();
+        aluno2.setNome("Daniel");
+
+        Aluno aluno3 = new Aluno();
+        aluno3.setNome("Sakamoto");
+
+        
+
+        alunoService.salvar(aluno1);
+        alunoService.salvar(aluno2);
+        alunoService.salvar(aluno3);
+
+        
+        List<Aluno> listaAlunos = alunoService.buscarTodosAlunos();
+
+        for (Aluno aluno : listaAlunos) {
+            System.out.println(aluno.getNome());
+        }
+        
+        //repo.saveAll(Arrays.asList(aluno1, aluno2, aluno3));
+
+
+    }
+}
