33b8713ad89f8efb2dc108b9d7cd15948fc92be9
ADS 2021.1N <36124872021.1N@senacrjedu.local>
2022-09-15 21:01:40 -0300

    feat: added cadastrar and salvar routes

--- src/main/java/br/com/senac/Controller/ProfessorController.java
+++ src/main/java/br/com/senac/Controller/ProfessorController.java
@@ -3,9 +3,11 @@ package br.com.senac.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.senac.entity.Professor;
 import br.com.senac.service.ProfessorService;
 
 @Controller
@@ -16,11 +18,24 @@ public class ProfessorController {
     private ProfessorService service;
 
     @GetMapping("listar")
-    public ModelAndView listarTodosAlunos(){
+    public ModelAndView listarTodosProfessores(){
         ModelAndView mv = new ModelAndView("professor/paginaListaProfessores");
         mv.addObject("professores", service.buscarTodosProfessors());
 
         return mv;
+    }
+
+    @GetMapping("/cadastrar") // http://localhost:8080/professor/cadastrar
+    public ModelAndView cadastrarProfessor(){
+        ModelAndView mv = new ModelAndView("Professor/cadastrarProfessor");
+        mv.addObject("professor", new Professor());
+        return mv;
+    }
+
+    @PostMapping("/salvar") // http://localhost:8080/professor/salvar
+    public ModelAndView salvarProfessor(Professor Professor){
+        service.salvar(Professor);
+        return listarTodosProfessores();
     }
 
 }
