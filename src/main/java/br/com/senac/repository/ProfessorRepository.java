660eac1a1c551cd88a9edd80592d0af9790af429
Victor Macedo <victormacedo996@gmail.com>
2022-11-14 20:54:55 -0300

    feat: added find prof book from prof service

--- src/main/java/br/com/senac/repository/ProfessorRepository.java
+++ src/main/java/br/com/senac/repository/ProfessorRepository.java
@@ -2,6 +2,7 @@ package br.com.senac.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.senac.entity.LivroL;
@@ -9,5 +10,6 @@ import br.com.senac.entity.Professor;
 
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Integer>{
-    
+    @Query(value = "SELECT l FROM LivroL l WHERE l.professor.id = ?1")
+    List<LivroL> buscarLivros(Integer id);
 }
